2830c0aadbe711fff07b6ab331860e93ad0b3111
FENG Hao <hao.feng@hpe.com>
2024-01-19 01:57:54 +0800

    fix🐛: bugs

--- core/admin.py
+++ core/admin.py
@@ -4,16 +4,15 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
 from django.utils.translation import gettext_lazy as _
 
+
 from django.conf.locale.en import formats as en_formats  # isort: skip
 en_formats.DATE_FORMAT = "Y-m-d"  # isort: skip
 
 User = get_user_model()
 
-admin.site.unregister(User)
 
+class UserCustomAdmin(UserAdmin):
 
-@admin.register(User)
-class MyUserAdmin(UserAdmin):
     def get_queryset(self, request):
         qs = super().get_queryset(request)
         if request.user.is_superuser:
@@ -23,20 +22,28 @@ class MyUserAdmin(UserAdmin):
     def get_fieldsets(self, request, obj=None):
         if not obj:
             return self.add_fieldsets
-        if not request.user.is_superuser:
-            fieldsets = (
-                (None, {'fields': ('username', 'password')}),
-                (_('Personal info'), {'fields': ('first_name', 'last_name', 'email')}),
-                (_('Permissions'), {
-                    'fields': ('is_active', 'is_staff', 'groups', 'user_permissions'),
-                }),
-                (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
-            )
-            if request.user.groups.filter(name='ATS Engineer').exists():
-                return (
-                    (None, {'fields': ('username', 'password')}),
-                    (_('Personal info'), {'fields': ('first_name', 'last_name', 'email')}),
-                )
-            return fieldsets
+        fieldsets = super().get_fieldsets(request, obj)
+        if request.user.is_superuser:
+            fieldsets[2][1]['fields'] = ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')
         else:
-            return self.fieldsets
+            fieldsets[2][1]['fields'] = ('is_active', 'is_staff', 'groups')
+        return fieldsets
+
+    def has_module_permission(self, request):
+        return request.user.is_staff
+
+    def has_view_permission(self, request, obj=None):
+        return request.user.is_staff
+
+    def has_add_permission(self, request):
+        return request.user.is_staff
+
+    def has_change_permission(self, request, obj=None):
+        return request.user.is_staff
+
+    def has_delete_permission(self, request, obj=None):
+        return request.user.is_staff
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserCustomAdmin)
